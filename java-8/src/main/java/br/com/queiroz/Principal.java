2d020a471a2410a64b98d6f97ca12e20e9ff6302
Alessandro Queiroz <queirozsilva.alessandro@gmail.com>
2016-12-01 23:30:09 -0200

    fontes Java 8

---
+++ java-8/src/main/java/br/com/queiroz/Principal.java
@@ -0,0 +1,57 @@
+package br.com.queiroz;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Principal {
+
+	
+	public static void main(String[] args) {
+		
+//		List<String> palavras = new ArrayList<>();
+//		palavras.add("alura online");
+//		palavras.add("casa do código");
+//		palavras.add("caelum");
+//		
+//		palavras.sort((s1, s2) ->  Integer.compare(s1.length(), s2.length()));
+//		
+//		palavras.forEach(p -> System.out.println(p));			
+//				
+//		new Thread(() -> System.out.println("Executando um Runnable")).start();
+//		
+//		Function<Usuario, String> funcao1 = u -> u.getNome();
+//		Function<Usuario, String> funcao2 = Usuario::getNome;
+//		
+//		//lambda
+//		palavras.sort((s1, s2) -> {
+//		    return Integer.compare(s1.length(), s2.length()); 
+//		});
+//		//lambda com comparing
+//		palavras.sort(Comparator.comparing(s -> s.length()));
+//		//reference com comparing
+//		palavras.sort(String.CASE_INSENSITIVE_ORDER);
+//
+
+	    
+	    List<Curso> cursos = new ArrayList<Curso>();
+	    cursos.add(new Curso("Python", 45));
+	    cursos.add(new Curso("JavaScript", 150));
+	    cursos.add(new Curso("Java 8", 113));
+	    cursos.add(new Curso("C", 55));
+	    
+	    //comparar cursos
+//	    cursos.sort(Comparator.comparing(curso -> curso.getAlunos()));
+//	    
+//	    cursos.sort(Comparator.comparing(Curso::getAlunos));
+//		
+//	    cursos.forEach(System.out::println);
+	    
+//		cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(c -> c.getAlunos()).forEach(x -> System.out.println(x));
+	    
+	    int soma = cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(c -> c.getAlunos()).sum();
+	    
+	    System.out.println(soma);
+	}
+}
---
+++ java-8/src/main/java/br/com/queiroz/Usuario.java
@@ -0,0 +1,9 @@
+package br.com.queiroz;
+
+public class Usuario {
+
+	public String getNome() {
+		return null;
+	}
+
+}
