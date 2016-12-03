de1b977d3ff5b757beebdd303f765fdc289f8c26
Alessandro Queiroz <queirozsilva.alessandro@gmail.com>
2016-12-02 23:12:12 -0200

    Altrações

--- java-8/src/main/java/br/com/queiroz/Principal.java
+++ java-8/src/main/java/br/com/queiroz/Principal.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Principal {
 
-	
 	public static void main(String[] args) {
 		
 //		List<String> palavras = new ArrayList<>();
@@ -50,8 +52,16 @@ public class Principal {
 	    
 //		cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(c -> c.getAlunos()).forEach(x -> System.out.println(x));
 	    
-	    int soma = cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(c -> c.getAlunos()).sum();
+//	    cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(c -> c.getAlunos()).sum();
 	    
-	    System.out.println(soma);
+//	    Optional<Curso> resultado = cursos.stream().filter(c -> c.getAlunos() >= 100).findFirst();
+//	    resultado.ifPresent(r -> System.out.println(r.getNome()));	
+	    
+//	    int resultado = cursos.stream().mapToInt(c -> c.getAlunos()).sum();
+//	    System.out.println(resultado);
+	    
+	    List<Curso> resultado = cursos.stream().filter(c -> c.getAlunos() > 50).collect(Collectors.toList());
+	    resultado.forEach(r -> System.out.println(r.getAlunos()));    
+
 	}
 }
