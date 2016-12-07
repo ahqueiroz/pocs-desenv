5644ded20ba5a0def50caae082b7f479a56f808d
Alessandro Queiroz <queirozsilva.alessandro@gmail.com>
2016-12-07 08:15:42 -0200

    alterações em auron

---
+++ auron/src/main/java/org/auron/service/exception/SorteioException.java
@@ -0,0 +1,10 @@
+package org.auron.service.exception;
+
+public class SorteioException extends Exception{
+
+	private static final long serialVersionUID = -8927010717452220805L;
+
+	public SorteioException(String message){
+		super(message);
+	}	
+}
