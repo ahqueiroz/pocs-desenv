cd791a62261bdbe3000098e043efbb4efeab88dc
Alessandro Queiroz <queirozsilva.alessandro@gmail.com>
2016-12-14 10:27:49 -0200

    reorganizando pacotes

--- livraria-vip/src/main/java/br/com/queiroz/livrariavip/repository/ConvidadoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.queiroz.livrariavip.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.queiroz.livrariavip.model.Convidado;
-
-public interface ConvidadoRepository extends CrudRepository<Convidado, Long>{
-
-}
