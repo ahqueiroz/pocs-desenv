705b666c7653fc9c6f8a8391d9d3c6166ec24708
Alessandro Queiroz <queirozsilva.alessandro@gmail.com>
2017-01-29 08:51:07 -0200

    tornando o projeto responsivo

--- curso-spring-mvc/cobranca/src/main/java/br/com/queiroz/cobranca/model/repository/filter/TituloFilter.java
+++ curso-spring-mvc/cobranca/src/main/java/br/com/queiroz/cobranca/model/repository/filter/TituloFilter.java
@@ -2,9 +2,13 @@ package br.com.queiroz.cobranca.model.repository.filter;
 
 public class TituloFilter {
 	
-	private String descricao = "%";
+	private String descricao;
 
 	public String getDescricao() {
+		
+		if (descricao == null)
+			return "%";
+		
 		return descricao;
 	}
 
